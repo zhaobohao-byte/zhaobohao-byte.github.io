f18f8c963bd682700cc8bbbbd0d4c3afe87a5772
bohao <1653505623@qq.com>
2025-04-16 00:57:31 +0800

    Site updated: 2025-04-16 00:57:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/14/test/","2025/04/14/示例一个md文件/","2025/04/14/使用Overleaf快速管理参考文献（基于TIE格式要求）/","2025/04/14/hello-world/"];function toRandomPost(){
+var posts=["2025/04/14/hello-world/","2025/04/14/test/","2025/04/14/使用Overleaf快速管理参考文献（基于TIE格式要求）/","2025/04/14/示例一个md文件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
