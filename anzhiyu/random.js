232abf0bcdf6125de43083ba4f8e6f9b3a57baff
bohao <1653505623@qq.com>
2025-04-16 15:47:39 +0800

    Site updated: 2025-04-16 15:47:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/16/latex论文写作-（三）/","2025/04/14/latex论文写作-（五）/"];function toRandomPost(){
+var posts=["2025/04/16/latex论文写作-（三）/","2025/04/14/latex论文写作-（五）/","2025/04/16/控制算法（一）-粒子群算法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
