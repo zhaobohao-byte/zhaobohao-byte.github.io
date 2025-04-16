11a0a9943279c504fb94e4abb7ff832f2bf69469
bohao <1653505623@qq.com>
2025-04-16 13:59:26 +0800

    Site updated: 2025-04-16 13:59:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/14/test/","2025/04/14/hello-world/","2025/04/14/示例一个md文件/","2025/04/14/使用Overleaf快速管理参考文献（基于TIE格式要求）/"];function toRandomPost(){
+var posts=["2025/04/16/latex论文写作-（三）/","2025/04/14/latex论文写作-（五）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
