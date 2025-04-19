c028aa06c871383e5434587190db8eb3d124e077
bohao <1653505623@qq.com>
2025-04-19 17:05:51 +0800

    Site updated: 2025-04-19 17:05:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/fe09d13e.html","posts/a00d6121.html","posts/78bebd03.html"];function toRandomPost(){
+var posts=["posts/a00d6121.html","posts/78bebd03.html","posts/fe09d13e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
