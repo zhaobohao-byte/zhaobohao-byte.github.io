f2453bd972baa3c92bd3e72c4719bdc67883faf7
bohao <1653505623@qq.com>
2025-04-19 22:09:57 +0800

    Site updated: 2025-04-19 22:09:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/78bebd03.html","posts/a00d6121.html","posts/fe09d13e.html"];function toRandomPost(){
+var posts=["posts/a00d6121.html","posts/78bebd03.html","posts/fe09d13e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
