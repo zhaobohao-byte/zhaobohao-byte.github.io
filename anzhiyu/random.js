df0d463af23462f50d0a2af0ebab446356ac1d3f
bohao <1653505623@qq.com>
2025-04-27 23:13:39 +0800

    Site updated: 2025-04-27 23:13:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d30087eb.html","posts/951318a8.html","posts/78bebd03.html","posts/18e3e2ea.html","posts/fe09d13e.html","posts/a00d6121.html","posts/6889b061.html","posts/30724d9a.html"];function toRandomPost(){
+var posts=["posts/951318a8.html","posts/18e3e2ea.html","posts/a00d6121.html","posts/d30087eb.html","posts/78bebd03.html","posts/fe09d13e.html","posts/6889b061.html","posts/30724d9a.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
